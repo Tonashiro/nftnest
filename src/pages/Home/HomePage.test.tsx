dc3ce9d2f9039227db4fade631ebd3492fd1e0c4
VInicius Tonashiro <vinit.tonashiro@hotmail.com>
2023-02-24 00:26:40 -0300

    feat: Create initial home page

---
+++ src/pages/Home/HomePage.test.tsx
@@ -0,0 +1,5 @@
+describe("HomePage Unit Tests", () => {
+  it("Should render HomePage", () => {});
+});
+
+export {};
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../components/Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <div data-testid="homepage">
+      <Navbar />
+    </div>
+  );
+};
+
+export { HomePage };
