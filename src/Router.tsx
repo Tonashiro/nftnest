9d8f07998f5786e1ce472ee9da0ecad60c64db71
VInicius Tonashiro <vinit.tonashiro@hotmail.com>
2023-03-11 22:09:39 -0300

    feat: Add HomePage to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,26 @@
 import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-const HomePage = lazy(() => import("./pages/Home/HomePage").then((module) => {
-  return { default: module.HomePage };
-}));
+const HomePage = lazy(() =>
+  import("./pages/Home/HomePage").then((module) => {
+    return { default: module.HomePage };
+  })
+);
 
 const Router = (): JSX.Element => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <HomePage />
-          </Suspense>
-        }
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/nfts" element={<h1>My NFTS</h1>} />
+        <Route path="/create-contract" element={<h1>Create Contract</h1>} />
+        <Route path="/create-nft" element={<h1>Upload NFT</h1>} />
+        <Route path="/mint" element={<h1>Mint NFT</h1>} />
+        <Route path="/update-nft" element={<h1>Update NFT</h1>} />
+        <Route path="/transfer-nft" element={<h1>Transfer NFT</h1>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Suspense>
   );
 };
 export { Router };
